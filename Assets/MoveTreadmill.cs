0283da76cf19734535a3ee844fde3ca188dc6807
ashlinaronin <ashlinaronin@gmail.com>
2021-01-14 22:36:58 -0800

    Try to move treadmill

---
+++ Assets/MoveTreadmill.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTreadmill : MonoBehaviour {
+
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 newOffset = new Vector2(0, rend.materials[0].GetTextureOffset("_MainTex").y + Time.deltaTime);
+		rend.materials[0].SetTextureOffset("_MainTex", newOffset);
+	}
+}
